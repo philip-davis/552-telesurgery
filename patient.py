c9f098ff59e36a1e3958ade85c2d0998d76368d5
Philip Davis <philip.e.davis@rutgers.edu>
2020-11-29 02:45:05 +0000

    Initial commit of telesurgery proxy

---
+++ patient.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python3
+
+"""https://stackoverflow.com/questions/33311153/python-extracting-and-saving-video-frames"""
+"""https://github.com/jeffbass/imagezmq/blob/master/examples/pub_sub_broadcast.py"""
+
+import sys
+from shutil import copyfile
+import cv2
+import time
+import threading
+import imagezmq
+import numpy as np
+import zmq
+
+b = threading.Barrier(2)
+
+class VideoStreamer:
+    def __init__(self, name, port):
+        self.port = port
+        stage = '/tmp/{file}'.format(file=name)
+        copyfile(name, stage)
+        self.vid = cv2.VideoCapture(stage)
+        self._sender = imagezmq.ImageSender('tcp://*:{port}'.format(port=self.port), REQ_REP=False)
+        self._done = threading.Event()
+        self._thread = threading.Thread(target=self._run, args=())
+        self._thread.daemon = True
+        self._thread.start()
+    
+    def _run(self):
+        time.sleep(5)
+        success, image = self.vid.read()
+        self.time = time.time()
+        count = 1
+        while success:
+            success, image = self.vid.read()
+            b.wait()
+            if success:
+                self._sender.send_image(str(count), image)
+            count += 1
+            self.time += .033
+            sleep_time = self.time - time.time()
+            if sleep_time > 0:
+                time.sleep(sleep_time)               
+        done_image = np.zeros(shape=[1,1,3], dtype=np.uint8)
+        self._sender.send_image("done", done_image)
+        print("done")
+        self._sender.close()
+        self._done.set()
+
+    def close(self):
+        flag = self._done.wait()
+
+
+class VideoStreamerCombined:
+    def __init__(self, lname, lport, rname, rport):
+        self.lport = lport
+        self.rport = rport
+        left_stage = '/tmp/{file}'.format(file=lname)
+        right_stage = '/tmp/{file}'.format(file=rname)
+        copyfile(lname, left_stage)
+        copyfile(rname, right_stage)
+        self.l_vid = cv2.VideoCapture(left_stage)
+        self.r_vid = cv2.VideoCapture(right_stage)
+        self._done = threading.Event()
+        self._thread = threading.Thread(target=self._run, args=())
+        self._thread.daemon = True
+        self._thread.start()
+    
+    def _run(self):
+        l_sender = imagezmq.ImageSender('tcp://*:{port}'.format(port=self.lport), REQ_REP=False)   
+        r_sender = imagezmq.ImageSender('tcp://*:{port}'.format(port=self.rport), REQ_REP=False)
+        time.sleep(5)
+        l_success, image = self.l_vid.read()
+        r_success, image = self.r_vid.read()
+        self.time = time.time()
+        count = 1
+        while l_success and r_success:
+            l_success, l_image = self.l_vid.read()
+            r_success, r_image = self.r_vid.read()
+            if l_success and r_success:
+                l_sender.send_image(str(count), l_image)
+                r_sender.send_image(str(count), r_image)
+            count += 1
+            self.time += .033
+            sleep_time = self.time - time.time()
+            if sleep_time > 0:
+                time.sleep(sleep_time)
+        done_image = np.zeros(shape=[1,1,3], dtype=np.uint8)
+        l_sender.send_image("done", done_image)
+        r_sender.send_image("done", done_image)
+        l_sender.close()
+        r_sender.close()
+        print("done")
+        self._done.set()
+
+    def close(self):
+        flag = self._done.wait()
+
+if len(sys.argv) !=3:
+    print('Usage: client.py <video file> <video file>')
+    exit(-1)
+
+#camera = VideoStreamerCombined(sys.argv[1], 5555, sys.argv[2], 5556)
+#camera.close()
+l_camera = VideoStreamer(sys.argv[1], 5555)
+r_camera = VideoStreamer(sys.argv[2], 5556)
+
+context = zmq.Context()
+sub = context.socket(zmq.SUB)
+sub.connect('tcp://localhost:5557')
+sub.setsockopt_string(zmq.SUBSCRIBE, '')
+
+lost = 0
+last = 0
+while(True):
+    count = int(sub.recv_string().split()[0])
+    if count == -1:
+        break
+    if(count > last + 1):
+        lost += count - (last + 1)
+    last = count 
+l_camera.close()
+r_camera.close()
+
+print('lost {} commands'.format(lost))
---
+++ surgeon.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+
+"""https://github.com/jeffbass/imagezmq/blob/master/examples/pub_sub_receive.py"""
+"""https://zguide.zeromq.org/docs/chapter1/"""
+
+import sys
+
+import cv2
+import imagezmq
+import threading
+import time
+import zmq
+from random import random
+
+class SurgeonControl:
+    def __init__(self, port):
+        self.port = port
+        ctx = zmq.Context.instance()
+        self._pub = ctx.socket(zmq.PUB)
+        self._pub.bind('tcp://*:{}'.format(port))
+        time.sleep(5)
+        self._done = False
+        self._thread = threading.Thread(target=self._run, args=())
+        self._thread.daemon = True
+        self._thread.start()
+
+    def _run(self):
+        time.sleep(5)
+        count = 0
+        while True:
+            count += 1
+            
+            self._pub.send_string('{count} {xshift},{yshift},{zshift}'.format(count=count, xshift=random(), yshift=random(), zshift=random()))
+            time.sleep(random())        
+            if self._done:
+                self._pub.send_string('-1 0 0 0')
+                break
+
+    def close(self):
+        self._done = True
+        self._thread.join()
+
+class VideoStreamSubscriber:
+
+    def __init__(self, hostname, port):
+        self.hostname = hostname
+        self.port = port
+        self._stop = False
+        self._data_ready = threading.Event()
+        self._thread = threading.Thread(target=self._run, args=())
+        self._thread.daemon = True
+        self._thread.start()
+
+    def receive(self):
+        flag = self._data_ready.wait()
+        if not flag:
+            raise TimeoutError(
+                "Timeout while reading from subscriber tcp://{}:{}".format(self.hostname, self.port))
+        self._data_ready.clear()
+        return self._recv_time, self._data
+
+    def _run(self):
+        receiver = imagezmq.ImageHub("tcp://{}:{}".format(self.hostname, self.port), REQ_REP=False)
+        while not self._stop:
+            self._data = receiver.recv_jpg()
+            self._recv_time = time.time()
+            self._data_ready.set()
+        receiver.close()
+
+    def close(self):
+        self._stop = True
+
+hostname = 'localhost'
+lport = 5555
+rport = 5556
+l_receiver = VideoStreamSubscriber(hostname, lport)
+r_receiver = VideoStreamSubscriber(hostname, rport)
+control = SurgeonControl(5557)
+
+count = 1
+diff = 0.
+lost = 0
+halflost = 0
+last = 0
+while True:
+    l_time, (l_msg, frame) = l_receiver.receive()
+    r_time, (r_msg, frame) = r_receiver.receive()
+    if l_msg != r_msg:
+        halflost += 1
+        if(int(l_msg) < int(r_msg)):
+            l_receiver.receive()
+        else:
+            r_receiver.receive()
+    if l_msg == "done" or r_msg == "done":
+        break
+    if(l_msg == r_msg):
+        frameno = int(l_msg)
+        if(frameno != last + 1):
+            print('lost {} frame(s)!'.format(frameno - last))
+            lost += (frameno - last)
+        last = frameno
+    if count == 1:
+        start_time = time.time()
+    #print("received frame pair ", count, abs(l_time-r_time))
+    if count % 100 == 0:
+        print('frame ', count)
+    diff += abs(l_time - r_time)
+    #print("received pair ", time.gmtime())
+    count += 1
+finish_time = time.time()
+control.close()
+print('{count} frames received in {time} seconds ({fps} FPS.)'.format(count=count, time=finish_time-start_time, fps=count/(finish_time-start_time)))
+print('average side disparity is {time} ms.'.format(time=(diff / count)*1000))
+print('lost {} frames and {} frame halves.'.format(lost, halflost))
