ba72d6e816c21bfa8c3b1d852e552523c3b2c184
root <root@node-0.pdavis-qv87944.novel-scheduler-pg0.utah.cloudlab.us>
2020-12-21 19:31:54 -0700

    Absoltue directories

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@
 
 sudo apt-get update
 sudo apt install git
-cd ..
+cd /root
 rm -rf 552-telesurgery-*
 git clone  https://github.com/philip-davis/552-telesurgery.git
+git --git-dir /root/552-telesurgery checkout postdev
